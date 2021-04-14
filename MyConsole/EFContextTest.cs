2a40b6dbcf17232ae83de89c3add492ed083e97a
探lu者 <529987528@qq.com>
2021-04-14 08:44:07 +0800

    upd...

--- MyConsole/EFContextTest.cs
+++ MyConsole/EFContextTest.cs
@@ -8,11 +8,15 @@ using System.Threading.Tasks;
 namespace MyConsole
 {
     /// <summary>
+    /// 能不能整个进程就一个context实例呀？当然不可以
+    /// 多线程能不能是一个context实例呢？一般来说不行(除非自己很明确状况)
+    /// 那每个数据操作都去来一个context实例？也不好，内存消耗大，没法缓存
+    /// 
     /// context使用建议：
-    /// DBcontext是一个上下文环境，里面内置对象跟踪，会开启一个链接(就等同于一个数据库链接)
-    /// 一次请求，最好是一个context；
-    /// 多个请求/多线程最好是多个实例；
-    /// 用完尽快释放；
+    /// DBcontext是一个上下文环境，里面内置对象跟踪，会开启一个连接(就等同于一个数据库连接)
+    /// 一次请求，最好是一个context实例；
+    /// 多个请求/多线程最好是多个context实例；
+    /// 用完尽快释放！！！
     /// </summary>
     public class EFContextTest
     {
@@ -72,16 +76,14 @@ namespace MyConsole
             #endregion
 
 
-            //能不能整个进程就一个context实例呀？当然不可以
-            //多线程能不能是一个实例呢？一般来说不行(除非自己很明确状况)
-            //那每个数据操作都去来一个context实例？也不好
-            //1 内存消耗大，没法缓存
-            //2 多context实例join行吗？不行，因为上下文环境不一样；除非把数据查到内存，在去linq
-            //3 多context的事务也麻烦
+            
+            //1 多context实例join行吗？不行，因为上下文环境不一样；除非把数据查到内存，在去linq
+            //2 多context的事务也麻烦
             using (LCDbContext dbContext1 = new LCDbContext())
             using (LCDbContext dbContext2 = new LCDbContext())
             {
                 //inner join
+                //这里会抛异常，因为context上下文环境不一样！
                 var list = (from t in dbContext1.tb_log
                             join c in dbContext2.tb_error on t.id equals c.id
                             where new long[] { 1, 2, 3, 4 }.Contains(t.id)
