d1c607ff96a9b58f8b91827148c9315c90cf495c
探lu者 <529987528@qq.com>
2021-04-15 13:59:20 +0800

    add...

--- MyConsole/Program.cs
+++ MyConsole/Program.cs
@@ -11,11 +11,12 @@ namespace MyConsole
     /// EF6->EntityFramework6：支持多种数据库；支持函数、存储过程等...
     /// 1、ORM对象关系映射
     /// 2、EntityFramework DBFirst
-    /// 3、EntityFramework CodeFirst from db 和 CodeFirst
+    /// 3、EntityFramework CodeFirst from db --推荐
+    /// 4、CodeFirst
     /// 4、EntityFramework modelFirst
     /// 5、EF增删改查基本实现
     /// 
-    /// context---映射数据库实例
+    /// context---映射数据库连接实例
     /// 实体类---映射表
     /// 
     /// 
@@ -28,28 +29,21 @@ namespace MyConsole
     ///          无
     /// ModelFirst：自己设计器里面设计表，然后在更新同步到数据库中
     ///           ModelFirst创建流程：项目右键-》添加-》新建项-》数据-》ADO.NET实体数据模型-》空EF设计器模型-》...
-    ///        
     /// 
     /// EF查看sql有2种方式：方式一：数据库管理工具里面的sql server profiler；方式二：dbContext.Database.Log += sqlStr => Console.WriteLine($"当前执行sql：{sqlStr}");
-    /// 
-    /// 
     /// 
     /// 1 各种复杂查询&直接执行sql
     /// 2 EF状态跟踪，本地增删改查实现
     /// 3 Context生命周期，多种事务
     /// 4 EF延迟查询、导航属性加载&增加&删除
     /// 
-    /// 
     /// 封装类库--上端引用--引用EF--搬迁配置文件数据库连接字符串
-    /// 
     /// 
     /// EF四种事务：
     /// 1 单个SaveChanges本身就是一个事务;。
     /// 2 TransactionScope完成一个context的多次SaveChanges事务。
     /// 3 TransactionScope完成不同context实例的事务。
     /// 4 dbContext.Database.BeginTransaction()实现事务。
-    /// 
-    /// 
     /// 
     /// 1 理解分层架构
     /// 2 EF分层封装数据访问
